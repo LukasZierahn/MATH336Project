366b0175a4cecbd85d75bd9f0dbb6e64897fd5df
Lukas Zierahn <lukas@kappa-mm.de>
2019-11-21 20:13:04 +0000

    Initial Commit

---
+++ k-means/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  k-means
+//
+//  Created by Lukas Zierahn on 21.11.19.
+//  Copyright © 2019 Lukas Zierahn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
